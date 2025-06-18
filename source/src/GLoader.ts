58cce6bbc6a27989443630084ddd562c0324af0e
wingcd <wingcd@foxmail.com>
2025-06-18 21:30:15 +0800

    修复GLoader外部资源填充bug
    (cherry picked from commit ca4c9c688561f8a80212396554fd32b9508df9b4)
    
    # Conflicts:
    #	source/dist/fairygui.min.mjs
    #	source/dist/fairygui.mjs

--- source/src/GLoader.ts
+++ source/src/GLoader.ts
@@ -240,7 +240,6 @@ export class GLoader extends GObject {
         this.clearContent();
 
         this._content.spriteFrame = value;
-        this._content.type = Sprite.Type.SIMPLE;
         if (value != null) {
             this.sourceWidth = value.getRect().width;
             this.sourceHeight = value.getRect().height;
@@ -249,6 +248,7 @@ export class GLoader extends GObject {
             this.sourceWidth = this.sourceHeight = 0;
         }
 
+        this.updateFillType();
         this.updateLayout();
     }
 
@@ -262,6 +262,23 @@ export class GLoader extends GObject {
             this.loadFromPackage(this._url);
         else
             this.loadExternal();
+    }
+
+    private updateFillType() {
+        if (this._content.fillMethod == 0) {
+            if(this._contentItem) {
+                if (this._contentItem.scale9Grid)
+                this._content.type = Sprite.Type.SLICED;
+                else if (this._contentItem.scaleByTile)
+                    this._content.type = Sprite.Type.TILED;
+                else
+                    this._content.type = Sprite.Type.SIMPLE;
+            }else{
+                this._content.type = Sprite.Type.SIMPLE;
+            }            
+        }else{
+            this._content.type = Sprite.Type.FILLED;
+        }
     }
 
     private init(contentItem: PackageItem, itemURL: string, dirtyVersion: number) {
@@ -281,16 +298,7 @@ export class GLoader extends GObject {
             }
             else {
                 this._content.spriteFrame = <SpriteFrame>this._contentItem.asset;
-                if (this._content.fillMethod == 0) {
-                    if (this._contentItem.scale9Grid)
-                        this._content.type = Sprite.Type.SLICED;
-                    else if (this._contentItem.scaleByTile)
-                        this._content.type = Sprite.Type.TILED;
-                    else
-                        this._content.type = Sprite.Type.SIMPLE;
-                }else{
-                    this._content.type = Sprite.Type.FILLED;
-                }
+                this.updateFillType();
                 this._content.__update();
                 this.updateLayout();
             }
@@ -474,11 +482,11 @@ export class GLoader extends GObject {
 
     protected onExternalLoadSuccess(texture: SpriteFrame): void {
         this._content.spriteFrame = texture;
-        this._content.type = Sprite.Type.SIMPLE;
         this.sourceWidth = texture.getRect().width;
         this.sourceHeight = texture.getRect().height;
         if (this._autoSize)
             this.setSize(this.sourceWidth, this.sourceHeight);
+        this.updateFillType();
         this.updateLayout();
     }
 
